9ceea413901d964f01106e2aca41a438b5a891f1
Gabriel Aldo <gabrielaldo443@gmail.com>
2024-09-12 20:10:31 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,45 +11,38 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.println("Room number: ");
 		int number = sc.nextInt();
-		System.out.println("Check-in date (dd/MM/yyyy): " );
+		System.out.println("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("Check-out date (dd/MM/yyyy): " );
+		System.out.println("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-		
+
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error ion reservation: Check-out date must be after check-in date");
-		}
-		else {
+		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
-			System.out.println("Check-in date (dd/MM/yyyy): " );
+			System.out.println("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
-			System.out.println("Check-out date (dd/MM/yyyy): " );
+			System.out.println("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error ion reservation: Check-out date must be after check-in date");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error ion reservation: Check-out date must be after check-in date");
 
-			}
-			else {
-
-				reservation.updateDates(checkIn, checkOut);
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservatrion: " + error);
+			} else {
 				System.out.println("Reservation: " + reservation);
+
 			}
 		}
-		
+
 	}
 
 }
