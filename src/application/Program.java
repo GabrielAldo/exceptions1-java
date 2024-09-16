efc19224ea6951c0f40ec57711f0db19bf7845a9
Gabriel Aldo <gabrielaldo443@gmail.com>
2024-09-16 20:21:27 -0300

    Good Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,11 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.println("Room number: ");
+		System.out.print("Room number: ");
 		int number = sc.nextInt();
-		System.out.println("Check-in date (dd/MM/yyyy): ");
+		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("Check-out date (dd/MM/yyyy): ");
+		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 
 		if (!checkOut.after(checkIn)) {
@@ -28,10 +28,10 @@ public class Program {
 			System.out.println("Reservation: " + reservation);
 
 			System.out.println();
-			System.out.println("Enter data to update the reservation: ");
-			System.out.println("Check-in date (dd/MM/yyyy): ");
+			System.out.print("Enter data to update the reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
-			System.out.println("Check-out date (dd/MM/yyyy): ");
+			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
 			String error = reservation.updateDates(checkIn, checkOut);
